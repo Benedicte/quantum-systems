9bd8709d4a956535a2ca1e58f12b915e893deb57
Benedicte <benediof@gmail.com>
2021-04-30 13:25:54 +0200

    fixed quantum systems

--- quantum_systems/__init__.py
+++ quantum_systems/__init__.py
@@ -2,12 +2,12 @@ from .system import QuantumSystem
 from .general_orbital_system import GeneralOrbitalSystem
 from .spatial_orbital_system import SpatialOrbitalSystem
 from .basis_set import BasisSet
-from .quest_system import CustomSystem
 from .custom_system import (
     # construct_psi4_system,
     # construct_pyscf_system,
     construct_pyscf_system_ao,
     construct_pyscf_system_rhf,
+    construct_quest_system_rhf
 )
 from .random_basis import RandomBasisSet
 from quantum_systems.sinc_dvr.one_dim.sinc_dvr import ODSincDVR
